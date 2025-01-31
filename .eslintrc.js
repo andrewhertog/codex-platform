b74f3b715387ae4b1b21e142d1e46c41560c126f
Ben Scholtens <ben.scholtens@me.com>
2025-01-31 12:45:51 -0500

    finaly figure out a way to get theia plugins working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-    root: true,
-    extends: [
-        './configs/base.eslintrc.json',
-        './configs/warnings.eslintrc.json',
-        './configs/errors.eslintrc.json',
-        './configs/xss.eslintrc.json'
+  root: true,
+  extends: [
+    "./configs/base.eslintrc.json",
+    "./configs/warnings.eslintrc.json",
+    "./configs/errors.eslintrc.json",
+    "./configs/xss.eslintrc.json",
+  ],
+  ignorePatterns: ["**/{node_modules,lib}", "plugins"],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      "./configs/tsconfig.eslint.json",
+      "./theia-extensions/*/tsconfig.json",
+      "applications/electron/tsconfig.eslint.json",
+      "./theia-plugins/*/tsconfig.json",
     ],
-    ignorePatterns: [
-        '**/{node_modules,lib}',
-        'plugins'
-    ],
-    parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: ['./configs/tsconfig.eslint.json', './theia-extensions/*/tsconfig.json', 'applications/electron/tsconfig.eslint.json']
-    }
+  },
 };
